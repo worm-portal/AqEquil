54bbcca44c6f9177c0e220e5098c4674fe508c57
gmboyer <gmboyer@asu.edu>
2022-09-16 14:25:00 -0700

    fixed activity coefficient units to be dimensionless

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AqEquil",
-    version="0.15.9",
+    version="0.15.10",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Python tools for aqueous chemical speciation.",
