bdde5034d999173382e6d63336b91b44d5d3cd1a
gmboyer <gmboyer@asu.edu>
2022-10-13 16:45:18 -0700

    updated docs and metadata after dev branch merge

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AqEquil",
-    version="0.15.11",
+    version="0.15.12",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Python tools for aqueous chemical speciation.",
