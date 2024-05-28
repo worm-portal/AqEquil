337d43c98e5c334f344b7bcaeca5b7bdcd071112
gmboyer <gmboyer@asu.edu>
2024-05-28 16:26:34 -0700

    updated CITATION and setup.py to reflect current package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AqEquil",
-    version="0.18.0",
+    version="0.19.0",
     author="Grayson Boyer",
     author_email="gmboyer@asu.edu",
     description="Python tools for aqueous chemical speciation.",
